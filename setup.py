64bcc2c51d9ef53aea0cce9359bb5b4671b7477e
chocolatejade42 <Aiden.Gardner@icloud.com>
2019-01-29 19:24:45 +1100

    Merge util.py file into setup.py - neaten up new setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,110 @@
-# Imported at the beginning, referenced to but not changed
-
+# Importing
 import os
 import praw
+import time
+import random
+import discord
 
+# Message setup function
+async def message_setup(m, client):
+    _devs = [
+        await client.get_user_info(id)
+        for id in [
+            272967064531238912,  # Me
+            270138433370849280,  # Jensen
+            297229962971447297,  # Jack
+            499740673424097303,  # Alt
+        ]
+    ]
+
+    _admin = m.author in _devs
+    _msg = m.content
+    _cmd = _msg.split()[0][1:].lower()
+    _args = _msg.split()[1:]
+
+    return _devs, _admin, _msg, _cmd, _args
+
+# Change bot status
+async def change_status(client, dev, *message):
+    status = random.choice([
+        # Playing
+        (0, "Tetris"),
+        # (0, "█▀█ █▄█ ▀█▀"),
+        (0, "with ya mum (We're playing connect 4, what did you think?)"),
+
+        # Streaming
+        (1, "¯\_(ツ)_/¯"),
+        (1, "(╯°□°）╯︵ ┻━┻"),
+        (1, "┬─┬ ノ( ゜-゜ノ)"),
+
+        # Listening to
+        (2, "ASMR"),
+        (2, "the voices in my head"),
+        (2, "desu desu desu 10 hours"),
+        (2, "my dev banging his head on the keyboard"),
+        
+        # Watching
+        (3, "out for nonces"),
+        (3, f"{dev} code!"),
+        (3, "Hamezza on YouTube"),
+        # (3, "Hentai with Jensen"),
+    ])
+
+    status = (status[0], status[1]+ ' |~| $help')
+    if message: status = (status[0], '%s |~| %s' % (status[1], message[0]))
+
+    await client.change_presence(
+        game= discord.Game(
+            name= status[1],
+            type= status[0],
+            url= "https://twitch.tv/chocolatejade42",
+        ))
+    
+    return status # Type tuple
+
+# 'cmd': ['Description of help message', [Aliases], classifier]
+CMDS = AttrDict({
+    # General
+    'help': ['Your average help message', [None], 'general'],
+    'info': ['Stats about the bot', [None], 'general'],
+    'status': ['Change my status :hugging:', [None], 'general'],
+    'invite': ['Invite links for the bot', ['invites'], 'general'],
+    'vote': ['Vote for the bot on Discord bot lists', ["upvote"], 'general'],
+    
+    # DM commands
+    'suggest': ['Suggest stuff for the bot and report bugs idk', ['bug'], 'dm'],
+
+    # Roast
+    'roast': ['Utterly obliviate someone\'s self-esteem', ['burn', 'feelsbadman'], 'roast'],
+
+    # Meme
+    'reddit': ['Get the freshest memes around', ['meme'], 'meme'],
+
+    # Text
+    'partyparrot': ['Send kewl messages with <a:partyparrot:538925147634008067>', ['party'], 'text'],
+
+    # Fun commands
+    '8ball': ['Ask the all-mighty, all-knowing :8ball:!', [None], 'fun'],
+    'spr': ['Play spr!', [None], 'fun'],
+    'ttt': ['Play Tic Tac Toe!', ['tictactoe'], 'fun'],
+})
+
+CMD_CLASSES= list(set([CMDS[command][2] for command in CMDS]))
+
+# Load .env for local testing
 try:
     import dotenv
-    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env")) # Load .env file for local testing
-except ModuleNotFoundError: pass # 'dotenv' not in requirements.txt so this snippet won't run
+    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+except ModuleNotFoundError: pass
 
+# Setup reddit
 reddit = praw.Reddit(
     user_agent= "Reddit Searching for Savage Cabbage#3666",
     client_id= os.getenv("REDDIT_ID"),
     client_secret= os.getenv("REDDIT_TOKEN"),
 )
 
+# Emojis code
 class emojis:
     partyparrot = '<a:partyparrot:538925147634008067>'
 
@@ -22,6 +113,7 @@ class AttrDict(dict):
     def __getattr__(self, attr): return self[attr]
     def __setattr__(self, attr, value): self[attr] = value
 
+# Roasts list
 roasts = [
     # "Oof!",
     "Hecc off!",
@@ -47,6 +139,7 @@ roasts = [
     # "You're as straight as the pole your mum dances on"
 ]
 
+# Eightball answers for the 8ball command
 eightball_answers = [
     # Yes
     "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Duh, of course"
@@ -57,11 +150,10 @@ eightball_answers = [
 ]
 
 
+# Other variables and data setup
 commands_run= commands_run_not_admin= 0
 roasts_str= ""
-
 one_in_what= 15
 greetings= ["Hey", "Yo", "Wassup", "Oi"]
 roasts_no_bold= [roast.replace("**", "") for roast in roasts]
-for roast in roasts_no_bold:
-    roasts_str+= f"{roast}\n"
+for roast in roasts_no_bold: roasts_str+= f"{roast}\n"
