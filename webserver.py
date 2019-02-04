20013e223277541a886d6f0dbf33b268ad69a6bb
chocolatejade42 <Aiden.Gardner@icloud.com>
2019-02-04 18:14:27 +1100

    Add discord embed to webpage - Custom embed link for inside discord - Add /static/icon.jpg as a flask route (needed for embed above)

--- webserver.py
+++ webserver.py
@@ -1,6 +1,11 @@
 # Flask setup
-from flask import Flask, render_template
+from flask import Flask, send_file, render_template
 web = Flask('Savage Cabbage Website')
+
+# Logging setup
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 # Commands setup
 from setup import (
@@ -19,15 +24,25 @@ def index(): return render_template("index.html",
 @web.route('/invite') # Redirect link
 def invite(): return render_template("redirect.html",
     title= "Invite Savage Cabbage",
-    redirect= BOT_INVITE_LINK
+    redirect= BOT_INVITE_LINK,
+    embed_title= "Invite Savage Cabbage to your server!",
+    embed_description= "yay!",
 )
 
 @web.route('/server-invite') # Redirect link
 def server_invite(): return render_template("redirect.html",
     title= "Savage Cabbage Support Server",
-    redirect= SUPPORT_SERVER_INVITE
+    redirect= SUPPORT_SERVER_INVITE,
+    embed_title= "Join Savage Cabbage's support server!",
+    embed_description= "Join for help with any of the commands, to report a bug or to use the custom commands!",
+)
+
+@web.route('/static/icon') # For reference
+def icon(): return send_file("static/icon.jpg",
+    mimetype= 'image/gif'
 )
 
 
+# Yeet that online!
 if __name__ == "__main__":
     web.run()
